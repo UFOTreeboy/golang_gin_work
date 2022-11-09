4bbfa3feaa5cdf528c7e5c7876b00d138ed4784a
UFOTreeboy <jinn7431080@gmail.com>
2022-11-09 12:50:06 +0800

    2022_11_9_1_增加package以及連結。

--- main.go
+++ main.go
@@ -3,45 +3,9 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
-	"database/sql"
-    "fmt"
-    "log"
-
-	_"github.com/mattn/go-sqlite3"
 )
 
 func main() {
-
-
-    db, err := sql.Open("sqlite3", "test.db")
-
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    defer db.Close()
-
-    sts := `
-DROP TABLE IF EXISTS cars;
-CREATE TABLE cars(id INTEGER PRIMARY KEY, name TEXT, price INT);
-INSERT INTO cars(name, price) VALUES('Audi',52642);
-INSERT INTO cars(name, price) VALUES('Mercedes',57127);
-INSERT INTO cars(name, price) VALUES('Skoda',9000);
-INSERT INTO cars(name, price) VALUES('Volvo',29000);
-INSERT INTO cars(name, price) VALUES('Bentley',350000);
-INSERT INTO cars(name, price) VALUES('Citroen',21000);
-INSERT INTO cars(name, price) VALUES('Hummer',41400);
-INSERT INTO cars(name, price) VALUES('Volkswagen',21600);
-`
-    _, err = db.Exec(sts)
-
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    fmt.Println("table cars created")
-
-
 
 
 	router := gin.Default()
@@ -58,4 +22,5 @@ INSERT INTO cars(name, price) VALUES('Volkswagen',21600);
 		})
 	})
 	router.Run("127.0.0.1:8080")
+
 }
