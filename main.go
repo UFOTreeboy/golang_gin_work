c1bc76d26db0196441186eda7922ace2be6412da
UFOTreeboy <jinn7431080@gmail.com>
2022-11-14 23:22:58 +0800

    2022_11_14_1，JSON使用

--- main.go
+++ main.go
@@ -3,11 +3,39 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	//"errors"
 )
 
+type book struct{
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Author string  `json:"author"`
+	Quantity int   `json:"quantity"`
+}
+
+var books = []book{
+	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
+
+func getBooks(c*gin.Context){
+	c.IndentedJSON(http.StatusOK, books)
+
+}
+
+
+func main(){
+	router := gin.Default()
+	router.GET("/books",getBooks)
+	router.Run("localhost:8080")
+
+
+}
+
+
+/*
 func main() {
-
-
 	router := gin.Default()
 	router.LoadHTMLGlob("template/*")//讀取template的html
 
@@ -24,3 +52,4 @@ func main() {
 	router.Run("127.0.0.1:8080")
 
 }
+*/
